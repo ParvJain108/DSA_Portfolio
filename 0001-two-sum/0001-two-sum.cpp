d3e6b9dbf71124a7338420e18a30382aee6261b1
Parv Jain <parvjainn255@gmail.com>
2026-08-01 22:39:42 +0530

    Time: 3 ms (66.36%), Space: 15.1 MB (9.5%) - LeetHub

---
+++ 0001-two-sum/0001-two-sum.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& nums, int target) 
+    {
+        map<int, int> mpp;
+        int n = nums.size();
+        for( int i = 0; i < n; i++)
+        {
+            int num = nums[i];
+            int more = target - num;
+            if(mpp.find(more) != mpp.end())
+            {
+                return{i, mpp[more]};
+            }
+            mpp[num] = i;
+        }
+        return{};
+    }
+};
+
+// Synced seamlessly with LeetHub Pro
+// Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+// Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
